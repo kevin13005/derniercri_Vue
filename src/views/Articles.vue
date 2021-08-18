eaad70623dd63d68d670273dd6a94e8ae5191d1c
kevin <kevin.prevost69@gmail.com>
2021-08-18 16:40:35 +0200

    un peu de style

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>
+    <div class="articles">
         <h1>Les articles</h1>
         <ul>
             <li v-for="(item, index) in articles" :key="index">
                 <div>
-                    <router-link :to="{name: 'details', params: { slug: item.title, date: item.date }}">
-                        <h1>{{ item.title }} </h1>
+                    <router-link :to="{name: 'details', params: { slug: item.content, date: item.date }}">
+                        <h1>lien détail : {{ item.title }} </h1>
                     </router-link>
                         <p>{{ item.date }}</p>
                 </div>
@@ -35,6 +35,7 @@ export default {
                 item.date = element.date;
                 item.slug = element.slug;
                 item.title = element.title.rendered;
+                item.content = element.content.rendered;
                 this.articles.push(item)
                 })
             }
@@ -45,3 +46,32 @@ export default {
     }
 }
 </script>
+
+<style>
+.articles h1{
+    text-align: center;
+    margin-bottom: 50px;
+}
+.articles ul {
+    width: 70%;
+    margin: auto;
+}
+.articles ul li{
+    margin-bottom: 80px;
+    display: flex;
+    justify-content: center;
+}
+.articles ul li div{
+    background-color: green;
+    color: white;
+    width: 60%;
+    margin: auto;
+}
+.articles ul li div a{
+    color: black;
+    text-decoration: none;
+}
+.articles ul li div p{
+    text-align: center;
+}
+</style>
--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
+    <div class="details">
         <h1>Détails d'un article</h1>
-        <div>
-            <p>{{ slug }}</p>
-            <p>{{ date }}</p>
+        <div class="param">
+            <p>nom du pokemon et son lien api :{{ slug }}</p>
+            <p>date appel api : {{ date }}</p>
         </div>  
     </div>
 </template>
@@ -14,4 +14,30 @@ export default {
 }
 </script>
 
-
+<style>
+.details{
+    background-color: green;
+    margin: auto;
+    height: 800px;
+    width: 70%;
+}
+.details h1 {
+    text-align: center;
+    color: white;
+    padding-bottom: 50px;
+}
+.details .param{
+    background-color: black;
+    color: white;
+    height: 300px;
+    width: 40%;
+    margin-top: 30px;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+.details .param p{
+    text-align: center;
+}
+</style>
